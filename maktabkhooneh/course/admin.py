c6fff7291a105c70e0feeb06bc91f0b1a5e04422
mhmmdmous <muhammadmousaviar7@gmail.com>
2025-04-04 17:35:03 +0330

    M v1.1

---
+++ maktabkhooneh/course/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from course.models import Course, Teacher, Student, Category
+
+
+# Register your models here.
+admin.site.register(Course)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Category)
+
+
+
--- maktabkhooneh/maktabkhooneh/settings.py
+++ maktabkhooneh/maktabkhooneh/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+try:
+    from maktabkhooneh.localsettings import *
+except ImportError:
+    pass
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+xwhm++cbvd0gi$ak@=fm%xwl8&5ywc*s=nl9hu)f4zu*+wnck'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'course',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +81,11 @@ WSGI_APPLICATION = 'maktabkhooneh.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "maktab",
-        "USER": "admin",
-        "PASSWORD": "adminpassword",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
     }
 }
 
