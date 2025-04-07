5bb31a0be023e58617d9cdc0ed53826fa80e1fd9
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-04-08 00:43:32 +0330

    set local settings

--- maktabkhooneh/maktabkhooneh/settings.py
+++ maktabkhooneh/maktabkhooneh/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'maktabkhooneh.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "maktab",
-        "USER": "admin",
-        "PASSWORD": "amir112233",
+        "NAME": "DB_NAME",
+        "USER": "DB_USER",
+        "PASSWORD": "DB_PASSWORD",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
