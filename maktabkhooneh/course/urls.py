fa11babbfac44e73e6c21325e678de5a05bcc050
mhmmdmous <muhammadmousaviar7@gmail.com>
2025-04-10 11:08:19 +0330

    V 1.10

--- maktabkhooneh/course/urls.py
+++ maktabkhooneh/course/urls.py
@@ -4,5 +4,5 @@ from .views import user_profile, update_course, courselist
 urlpatterns = [
     path('profile/', user_profile),
     path("update-course/<int:course_id>/", update_course),
-    path('course-list', courselist.asview()),
+    path('course-list', courselist.as_view()),
 ]
